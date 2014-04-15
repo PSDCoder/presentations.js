5fd23f6c9744732001510266c0cc720bf2a2d37c
Pavel Grynchenko <psdcoder@gmail.com>
2014-04-15 12:32:54 +0400

    Added gulp build tool for minifying js and css

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var jshintStylish = require('jshint-stylish');
+var rename = require('gulp-rename');
+var csso = require('gulp-csso');
+var uglify = require('gulp-uglify');
+
+gulp.task('default', ['js', 'css'], function () {})
+    .task('js', function () {
+        return gulp.src(['lib/paginator.js', 'lib/presentations.js'])
+            .pipe(jshint())
+            .pipe(jshint.reporter(jshintStylish))
+            .pipe(uglify())
+            .pipe(rename(function (path) {
+                path.extname = '.min.js';
+            }))
+            .pipe(gulp.dest('build/'));
+    })
+    .task('css', function () {
+        return gulp.src('lib/presentations.css')
+            .pipe(csso())
+            .pipe(rename(function (path) {
+                path.extname = '.min.css';
+            }))
+            .pipe(gulp.dest('build/'));
+    });
